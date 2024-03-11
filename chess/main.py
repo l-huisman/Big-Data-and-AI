201df9bc744a3c850c43381c493a65ad22629b93
Luke Huisman <huisman.l@yahoo.com>
2024-03-11 13:38:36 +0100

    Add chess game files

---
+++ chess/main.py
@@ -0,0 +1,15 @@
+# Main class
+
+import pygame
+import sys
+import os
+import time
+import random
+import chessboard
+import chesspiece
+import chessmove
+import chessai
+import chessgame
+import chessconfig
+import chessmenu
+

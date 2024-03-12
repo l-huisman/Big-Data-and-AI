b7ab25d50c5b3d8bef3a8d86c124c165e80fe7a3
Luke Huisman <huisman.l@yahoo.com>
2024-03-12 13:19:47 +0100

    Add type hinting for get_piece_at_position method

--- chess/board.py
+++ chess/board.py
@@ -5,6 +5,7 @@ from constants import Color, PieceType
 from pieces import Piece, Pawn, Rook, Knight, Bishop, Queen, King
 
 import pygame
+from typing import Union
 
 
 class Board:
@@ -44,7 +45,7 @@ class Board:
         self.board[4][0] = King(self, Color.WHITE, 4, 0)
         self.board[4][7] = King(self, Color.BLACK, 4, 7)
 
-    def get_piece_at_position(self, x_position: int, y_position: int) -> Piece:
+    def get_piece_at_position(self, x_position: int, y_position: int) -> Union[Piece, None]:
         return self.board[x_position][y_position]
 
     def move_piece(self, original_position: tuple, new_position: tuple) -> None:

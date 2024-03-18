e26aa5c10f10027ede198003fcaee17ef902d348
Luke Huisman <huisman.l@yahoo.com>
2024-03-18 13:29:10 +0100

    Refactor piece movement logic in bishop.py, pawn.py, piece.py, queen.py, and rook.py

--- chess/constants.py
+++ chess/constants.py
@@ -3,15 +3,6 @@ from enum import Enum
 
 BOARD_SIZE = 8
 
-
-class PieceType(Enum):
-    KING = 0
-    QUEEN = 1
-    ROOK = 2
-    BISHOP = 3
-    KNIGHT = 4
-    PAWN = 5
-
 class Color(Enum):
     WHITE = -1
     BLACK = 1

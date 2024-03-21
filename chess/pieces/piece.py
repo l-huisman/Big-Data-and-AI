97b57689792f58b62f97da6e8e77b1ea8bd27003
Luke Huisman <huisman.l@yahoo.com>
2024-03-21 09:43:35 +0100

    Fix image path in draw method of Piece class

--- chess/pieces/piece.py
+++ chess/pieces/piece.py
@@ -23,7 +23,7 @@ class Piece:
 
     def draw(self, screen, square_size: int, y: int, x: int) -> None:
         image = pygame.image.load(
-            "chess/img/" + self.__class__.__name__ + "_" + self.color.name + ".png"
+            "./img/" + self.__class__.__name__ + "_" + self.color.name + ".png"
         )
         image = pygame.transform.scale(image, (square_size, square_size))
         screen.blit(image, (x * square_size, y * square_size))

d96c58ec32a92f8847851e8fdca144443c4f904d
Serena <Serena.kenter@gmail.com>
2024-04-24 12:22:36 +0200

    Update Chessboard.vue

--- frontend/components/Chessboard.vue
+++ frontend/components/Chessboard.vue
@@ -9,8 +9,10 @@
             'bg-[#AA8439]': (RowIndex + colIndex) % 2 !== 0
           }"
           class="w-[70px] h-[70px] flex justify-center items-center">
-          <span class="text-white">{{ square }}</span>
-            <img src="../assets/images/pieces/bishop black.png" alt="not work">
+          <!-- <span class="text-white">{{ square }}</span> -->
+            <span>
+              <img v-if="square !== 0" :src="getPieceImagePath(square)" alt="Chess Piece" class="w-full h-full absolute inset-0 w-[40px] h-[40px]">
+            </span>
         </div>
       </template>
     </template>
@@ -41,8 +43,15 @@ export default {
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0]
-      ]
-      
+      ],
+      pieceImages: {
+        1: '/_nuxt/assets/images/pieces/pawn black.png',
+        2: '/_nuxt/assets/images/pieces/rook black.png',
+        3: '/_nuxt/assets/images/pieces/knight black.png',
+        4: '/_nuxt/assets/images/pieces/bishop black.png',
+        5: '/_nuxt/assets/images/pieces/queen black.png',
+        6: '/_nuxt/assets/images/pieces/king black.png',
+      }
     };
   },
   mounted() {
@@ -54,18 +63,17 @@ export default {
         if (first64[i][j] != 0) {
           this.dict.push({
             // key: 'white',
+            // value: first64[i][j]
             'white': first64[i][j]
           });
         }
         else if (last64[i][j] != 0) {
           this.dict.push({
-            // color: 'black',
             'black': last64[i][j]
           });
         }
         else {
           this.dict.push({ 
-            // color: 'Empty',
             'Empty': first64[i][j]
           });
         }
@@ -75,6 +83,12 @@ export default {
 
     console.log(this.dict);
   },
+
+  methods: {
+    getPieceImagePath(pieceNumber) {
+      return this.pieceImages[pieceNumber];
+    }
+  }
 };
 </script>
 

8a73c24d36baaca2b320c6293edcb6bfeab2fe42
Serena <Serena.kenter@gmail.com>
2024-04-30 00:32:43 +0200

    playing is possible

--- frontend/pages/play.vue
+++ frontend/pages/play.vue
@@ -22,8 +22,8 @@
     </div>
     <div class="ml-[10%] text-white">
         <input v-model="this.move_request.move" type="text" placeholder="e.g. e2e4"
-            class="text-black px-4 rounded-[2px] h-[28px]">
-        <button @click="makeMove()">Move</button>
+            class="text-black px-4 rounded-[2px] h-[28px] mr-[20px]">
+        <button class="bg-[#123456] pl-[15px] pr-[15px] pb-[3px] pt-[3px]" @click="makeMove()">Move</button>
     </div>
 </template>
 
@@ -43,6 +43,7 @@ export default {
             ],
             boardKey: 0,
             recoursePoints: 0,
+            temporaryBoard: [],
             move_request: {
                 move: 'e2e4',
                 turn: 1,
@@ -73,23 +74,47 @@ export default {
         getImageUrl(imageName) {
             return `/_nuxt/assets/images/cards/${imageName}.png`;
         },
+        parseMove(move) {
+            const fileFrom = move.charCodeAt(0) - 'a'.charCodeAt(0);
+            const rankFrom = parseInt(move[1]) - 1;
+            const fileTo = move.charCodeAt(2) - 'a'.charCodeAt(0);
+            const rankTo = parseInt(move[3]) - 1;
+            return { fileFrom, rankFrom, fileTo, rankTo };
+        },
+        applyMove(move) {
+            const { fileFrom, rankFrom, fileTo, rankTo } = this.parseMove(move);
+            const piece = this.move_request.board[1][rankFrom][fileFrom];
+            this.move_request.board[1][rankTo][fileTo] = piece;
+            this.move_request.board[1][rankFrom][fileFrom] = 0;
+        },
+        undoMove(move) {
+            const { fileFrom, rankFrom, fileTo, rankTo } = this.parseMove(move);
+            const piece = this.move_request.board[1][rankTo][fileTo];
+            this.move_request.board[1][rankFrom][fileFrom] = piece; 
+            this.move_request.board[1][rankTo][fileTo] = 0; 
+        },
         makeMove() {
+            this.applyMove(this.move_request.move)
+            this.boardKey++;
             this.move_request.board[0].reverse();
-            console.log(this.move_request.move);
-            console.log(this.move_request.turn);
-            // console.log(this.move_request.board);
-            axios.post('http://127.0.0.1:8000/move', this.move_request)
-                .then(response => {
-                    this.move_request.board = response.data.board;
-                    // this.move_request.turn += 1;
-                    this.recoursePoints += 1;
-                    this.move_request.move = '';
-                    this.boardKey++;
-                })
-                .catch(error => {
-                    console.error('Error making move:', error.response.data.detail);
-                });
-            console.log(this.move_request.board);
+
+            setTimeout(() => {
+                this.undoMove(this.move_request.move)
+                this.move_request.board[0].reverse();
+                console.log(this.move_request.board)
+
+                axios.post('http://127.0.0.1:8000/move', this.move_request)
+                    .then(response => {
+                        this.move_request.board = response.data.board;
+                        // this.move_request.turn += 1;
+                        this.recoursePoints += 1;
+                        this.move_request.move = '';
+                        this.boardKey++;
+                    })
+                    .catch(error => {
+                        console.error('Error making move:', error.response.data.detail);
+                    });
+            }, 2000);
         }
     }
 };

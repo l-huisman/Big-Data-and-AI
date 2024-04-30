f43764949f5330e83481fadda7aa4208fc12bc90
Serena <Serena.kenter@gmail.com>
2024-04-30 12:17:58 +0200

    errormessage works

--- frontend/pages/play.vue
+++ frontend/pages/play.vue
@@ -6,7 +6,8 @@
         <div class="ml-[50px] flex flex-col w-[40%]">
             <div class="flex flex-row justify-between h-[100px] text-2xl">
                 <div>Turn 1</div>
-                <div>Your move</div>
+                <div v-if="pointsBoard1 >= pointsBoard2"> + {{ Math.abs(pointsBoard1 - pointsBoard2) }}</div>
+                <div v-else> - {{ Math.abs(pointsBoard1 - pointsBoard2) }}</div>
             </div>
             <div class="bla ml-[20px] flex flex-row  right-0 ml-auto mb-[12px] text-lg justify-end">
                 resource points: 0
@@ -24,6 +25,9 @@
         <input v-model="this.move_request.move" type="text" placeholder="e.g. e2e4"
             class="text-black px-4 rounded-[2px] h-[28px] mr-[20px]">
         <button class="bg-[#123456] pl-[15px] pr-[15px] pb-[3px] pt-[3px]" @click="makeMove()">Move</button>
+        <div class="text-[#db3d35]">
+            {{ this.errorMessage }}
+        </div>
     </div>
 </template>
 
@@ -37,6 +41,7 @@ export default {
     },
     data() {
         return {
+            errorMessage: '',
             imageRows: [
                 ['hoplite', 'hoplite', 'hoplite'],
                 ['winged knight', 'dutch waterline', 'war elefant']
@@ -45,7 +50,7 @@ export default {
             recoursePoints: 0,
             temporaryBoard: [],
             move_request: {
-                move: 'e2e4',
+                move: '',
                 turn: 1,
                 board: [[
                     [4, 3, 2, 6, 5, 2, 3, 4],
@@ -70,20 +75,24 @@ export default {
             }
         };
     },
+    computed: {
+        pointsBoard1() {
+            return this.calculateBoardPoints(this.move_request.board[1]);
+        },
+        pointsBoard2() {
+            return this.calculateBoardPoints(this.move_request.board[0]);
+        }
+    },
     methods: {
         getImageUrl(imageName) {
             return `/_nuxt/assets/images/cards/${imageName}.png`;
         },
-
         makeMove() {
-            // for (let i = 0; i < 8; i++) {
-            //     this.move_request.board[0][i].reverse();
-            // }
             this.move_request.board[0].reverse();
 
             axios.post('http://127.0.0.1:8000/move', this.move_request)
                 .then(response => {
-                    // this.move_request.turn += 1;
+                    this.errorMessage = '';
                     this.recoursePoints += 1;
                     this.move_request.move = '';
 
@@ -97,7 +106,44 @@ export default {
                 })
                 .catch(error => {
                     console.error('Error making move:', error.response.data.detail);
+                    this.errorMessage = error.response.data.detail;
+                    this.move_request.board[0].reverse();
                 });
+        },
+        calculatePiecePoints(piece) {
+            switch (piece) {
+                case 0:
+                    return 0; // Empty square
+                case 1:
+                    return 1; // Pawn
+                case 2:
+                    return 3; // Bishop
+                case 3:
+                    return 3; // Knight
+                case 4:
+                    return 5; // Rook
+                case 5:
+                    return 9; // Queen
+                case 6:
+                    return 5; // Winged Knight
+                case 7:
+                    return 3; // Hoplite
+                case 8:
+                    return 5; // War Elephant
+                case 9:
+                    return 0; // King
+                default:
+                    return 0; // Handle unknown pieces
+            }
+        },
+        calculateBoardPoints(board) {
+            let totalPoints = 0;
+            for (let row of board) {
+                for (let piece of row) {
+                    totalPoints += this.calculatePiecePoints(piece);
+                }
+            }
+            return totalPoints;
         }
     }
 };

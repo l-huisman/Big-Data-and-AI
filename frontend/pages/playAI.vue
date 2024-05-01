61ce67458dfc394f1bc5ab524a068689b0ec846b
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2024-05-01 15:52:49 +0200

    small update can choose model but unable to update chessboard

--- frontend/pages/playAI.vue
+++ frontend/pages/playAI.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="flex h-screen w-screen mt-[155px] text-white">
     <div class="ml-[10%] text-2xl">
-      <Chessboard :board="board"/>
+      <Chessboard :board="board" />
+      <!-- <Chessboard :board="board" @update:board="newState" /> -->
     </div>
     <div class="ml-[50px] flex flex-col w-[45%]" >
       <div class=" text-2xl mb-[10px]">
         <div>Turn 1</div>
       </div>
+      <div class="flex flex-row bg-[#5a9679] rounded-[5px] border-[7px] border-[#5a9679] mb-2">
+        <select v-model="fmodel" class="bg-[#5a9679]" name="firstmodel" id="firstmodel">
+          <option value="PPO">PPO</option>
+          <option value="DQN">DQN</option>
+        </select>
+        <select v-model="smodel" class="bg-[#5a9679]" name="secondmodel" id="secondmodel">
+          <option value="PPO">PPO</option>
+          <option value="DQN">DQN</option>
+        </select>
+        <button @click="fetchGameAIvsAI(fmodel, smodel)" class="w-full rounded-full bg-[#3B6651] p-2">Start AI game</button>
+      </div>
       <div class="flex flex-row bg-[#afe0c8] rounded-[5px] border-[7px] border-[#5a9679]">
         <div class="w-1/2">
-          <Line height="200px" :data="chartData" :options="chartOptions"></Line>
-          <Line height="200px" :data="chartData1" :options="chartOptions1"></Line>
+          <!-- <Line height="200px" :data="chartData" :options="chartOptions"></Line> -->
+          <!-- <Line height="200px" :data="chartData1" :options="chartOptions1"></Line> -->
         </div>
         <div class="w-1/2">
-          <Pie id="pie-chart" :options="pieChartOptions" :data="pieChartData" />
+          <!-- <Pie id="pie-chart" :options="pieChartOptions" :data="pieChartData" /> -->
         </div>
       </div>
     </div>
@@ -22,115 +34,16 @@
 
 <script>
 import Chessboard from '../components/Chessboard.vue';
-import axios from 'axios';
-
-import { Line, Pie } from 'vue-chartjs'
-import Chart from 'chart.js/auto';
 
 export default {
-  data() {
-    return {
-    }
-  },
   components: {
     Chessboard,
-    Line,
-    Pie
   },
   data() {
     return {
-      //win chances
-      chartData: {
-        labels: ['January', 'February', 'March', 'January', 'February'],
-        datasets: [
-          { data: [40, 20, 10, 40, 20,], label: 'White', borderColor: '#567cb8' },
-          { data: [30, 50, 10, 40, 10], label: 'Black', borderColor: '#94979c' }
-        ]
-      },
-      chartOptions: {
-        plugins: {
-          title: {
-            display: true,
-            text: 'Win chances per turn',
-          }
-        },
-        responsive: true,
-        scales: {
-          x: {
-            type: 'category',
-            labels: ['January', 'February', 'March', 'January', 'February'],
-            title: {
-              display: true,
-              text: 'Turns',
-            }
-          },
-          y: {
-            min: 0,
-            max: 100,
-            title: {
-              display: true,
-              text: '%',
-            }
-          }
-        }
-      },
-      //total rewards per turn
-      chartData1: {
-        labels: ['January', 'February', 'March', 'January', 'February'],
-        datasets: [
-          { data: [40, 20, 10, 40, 20,], label: 'White', borderColor: '#567cb8' },
-          { data: [30, 50, 10, 40, 10], label: 'Black', borderColor: '#94979c' }
-        ]
-      },
-      chartOptions1: {
-        plugins: {
-          title: {
-            display: true,
-            text: 'Total rewards per turn',
-          }
-        },
-        responsive: true,
-        scales: {
-          x: {
-            type: 'category',
-            labels: ['January', 'February', 'March', 'January', 'February'],
-            title: {
-              display: true,
-              text: 'Turns',
-            }
-          },
-          y: {
-            min: -300,
-            max: 300,
-            title: {
-              display: true,
-              text: 'Rewards',
-            }
-          }
-        }
-      },
-      //total of checks
-      pieChartData: {
-        labels: ['White', 'Black'],
-        datasets: [{
-          data: [70, 30],
-          backgroundColor: ['#567cb8', '#94979c']
-        }]
-      },
-      pieChartOptions: {
-        plugins: {
-          title: {
-            display: true,
-            text: 'Total of checks',
-          }
-        },
-        responsive: true
-      },
-      turn: 1,
-      imageRows: [
-        ['hoplite', 'hoplite', 'hoplite'],
-        ['winged knight', 'dutch waterline', 'war elefant']
-      ],
+      fmodel: 'PPO',
+      smodel: 'PPO',
+      newState: [],
       board: [[
                     [4, 3, 2, 6, 5, 2, 3, 4],
                     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -151,7 +64,8 @@ export default {
                     [0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0]]
                 ],
-    };
+    }
+
   },
   methods: {
     async fetchGameAIvsAI(white_model, black_model) {
@@ -167,6 +81,15 @@ export default {
       });
       const data = await response.json();
       console.log(data);
+      this.runAIvsAI(data.game);
+    },
+    // not working yet
+    runAIvsAI(game) {
+      for (let i = 1; i < game.length; i++) {
+        setTimeout(() => {
+          this.newState = game[i];
+        }, 1000);
+      }
     }
   }
 };

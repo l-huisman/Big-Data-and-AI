34e66dcfec6e53a8d6750722e554dbd47e3bc8e0
Tim Voorneveld <30288486+TimV22@users.noreply.github.com>
2024-05-13 16:06:23 +0200

    Reworked some more code !!The frontend ai vs player will break with this merge

--- backend/api/main.py
+++ backend/api/main.py
@@ -6,8 +6,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from agents import PPOChess
 from api.models.requests import MoveRequest, AIGameRequest, ActionRequest
-from api.models.responses import AIGameResponse
-from api.routes import Move, PlayableActions, Initialize
+from api.models.responses import AIGameResponse, InitializeResponse, MoveResponse, ActionResponse
+from api.routes import Move, PlayableActions, Initialize, AiGame
 from buffer.episode import Episode
 from chess import Chess
 from learnings.ppo import PPO
@@ -53,77 +53,25 @@ episode = Episode()
 ppo_chess = PPOChess(env, ppo, 1, 32, "", WHITE_PPO_PATH, BLACK_PPO_PATH)
 
 
-@app.get(path="/initialize", status_code=201)
+@app.get(path="/initialize", response_model=InitializeResponse)
 def initialize():
     route = Initialize.Initialize(env=env)
     return route.execute()
 
 
-@app.post(path="/move")
+@app.post(path="/move", response_model=MoveResponse)
 def move(move_request: MoveRequest):
     route = Move.Move(env=env, agent=ppo_chess, episode=episode, move_request=move_request)
     return route.execute()
 
 
-@app.get(path="/actions")
+@app.get(path="/actions", response_model=ActionResponse)
 def actions(action_request: ActionRequest):
     route = PlayableActions.PlayableActions(env=env, action_request=action_request)
     return route.execute()
 
 
-@app.post(path="/aigame", status_code=201)
+@app.post(path="/aigame", response_model=AIGameResponse)
 def aigame(ai_game_request: AIGameRequest):
-    logger.info(f"Received aigame request: {ai_game_request}")
-    response = AIGameResponse(game=[], statistics=[])
-    try:
-        env.reset()
-
-        match ai_game_request.white_model.capitalize():
-            case "PPO":
-                white_model = WHITE_PPO_PATH
-            case "DQN":
-                # white_model = WHITE_DQN_PATH
-                logger.info("DQN not implemented yet, using PPO instead.")
-                white_model = WHITE_PPO_PATH
-            case _:
-                white_model = WHITE_PPO_PATH
-
-        match ai_game_request.black_model.capitalize():
-            case "PPO":
-                black_model = BLACK_PPO_PATH
-            case "DQN":
-                # black_model = BLACK_DQN_PATH
-                logger.info("DQN not implemented yet, using PPO instead.")
-                black_model = BLACK_PPO_PATH
-            case _:
-                black_model = BLACK_PPO_PATH
-
-        ppo_chess = PPOChess(env, ppo, 1, 32, "", white_model, black_model)
-        episode = Episode()
-        logger.info("Initialized AI game.")
-
-        # Play the game
-        counter = 0
-        response.game.append(ppo_chess.env.board.tolist())
-        response.statistics.append({"rewards": [0, 0], "infos": [[], []], "end": False})
-
-        while True:
-            done, _ = ppo_chess.take_action(ppo_chess.env.turn, episode)
-            response.statistics.append({"rewards": _[1], "infos": _[7], "end": done})
-            response.game.append(ppo_chess.env.board.tolist())
-
-            counter += 1
-            if done:
-                logger.info("Game Over")
-                logger.info("Winner: White" if ppo_chess.env.turn else "Winner: Black")
-                logger.info("Game Length: ", counter)
-                break
-
-        logger.info(f"AI game completed.")
-        return response
-    except FileNotFoundError as e:
-        logger.error(f"Could not find model on specified location, make sure the location is correct. {e}")
-        raise HTTPException(status_code=500, detail="Could not find any model.")
-    except Exception as e:
-        logger.error(f"An error occurred while resetting the game. {e}")
-        raise HTTPException(status_code=500, detail="An error occurred while initializing the game.")
+    route = AiGame.AiGame(env=env, ai_game_request=ai_game_request)
+    return route.execute()
--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,3 +1,4 @@
 import api.routes.actions as PlayableActions
 import api.routes.initialize as Initialize
 import api.routes.move as Move
+import api.routes.ai_game as AiGame

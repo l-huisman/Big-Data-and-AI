2450e22e6637c839ccc643f910823bf67412faf3
l-huisman <64755533+l-huisman@users.noreply.github.com>
2024-06-14 14:52:23 +0200

    More environmental variables

--- backend/aivsai.py
+++ backend/aivsai.py
@@ -1,46 +1,40 @@
+import os
 import sys
 from time import sleep
 
 import pygame
+from dotenv import load_dotenv
 
 from agents import PlayAgent
-from buffer.episode import Episode
 from aow.game.aow import ArtOfWar
+from buffer.episode import Episode
 from learnings.ppo import PPO
 
-sys.setrecursionlimit(300)
-env = ArtOfWar(window_size=800, max_steps=256)
+load_dotenv()
+
+sys.setrecursionlimit(int(os.getenv("RECURSION_LIMIT")))
+env = ArtOfWar(window_size=int(os.getenv("AOW_WINDOW_SIZE")), max_steps=int(os.getenv("AOW_MAX_STEPS")))
 env.render()
 
 # Paths to your trained api
-white_ppo_path = 'results/DoubleAgentsPPO/white_dict.pt'
-black_ppo_path = 'results/DoubleAgentsPPO/black_dict.pt'
+white_ppo_path = os.getenv("PPO_RESULT_FOLDER") + '/white_dict.pt'
+black_ppo_path = os.getenv("PPO_RESULT_FOLDER") + '/black_dict.pt'
 
 ppo = PPO(
     env,
-    hidden_layers=(2048,) * 4,
-    epochs=100,
-    buffer_size=32 * 2,
-    batch_size=256,
+    hidden_layers=(int(os.getenv("PPO_HIDDEN_LAYERS_SIZE")),) * int(os.getenv("PPO_HIDDEN_LAYERS_COUNT")),
+    epochs=int(os.getenv("PPO_EPOCHS")),
+    buffer_size=int(os.getenv("BUFFER_SIZE")) * 2,
+    batch_size=int(os.getenv("BATCH_SIZE")),
+    gamma=float(os.getenv("PPO_GAMMA")),
+    gae_lambda=float(os.getenv("PPO_GAE_LAMBDA")),
+    policy_clip=float(os.getenv("PPO_POLICY_CLIP")),
+    learning_rate=float(os.getenv("PPO_LEARNING_RATE")),
 )
 
-# dqn = DQNLearner(
-#     env,
-#     epochs=100,
-#     gamma=0.99,
-#     learning_rate=0.003,
-#     hidden_layers=(2048,) * 4,
-#     buffer_size=32,
-#     batch_size=128,
-#     epsilon=0.1,
-#     epsilon_decay=0.995,
-#     epsilon_min=0.01,
-#     tau=0.99,
-#     update_every=4,
-# )
-
 # Create an instance of PlayAgent
-aow_game = PlayAgent(env, ppo, 1, 32, "", white_ppo_path, black_ppo_path)
+aow_game = PlayAgent(env, ppo, episodes=int(os.getenv("EPISODES")), train_on=int(os.getenv("BUFFER_SIZE")),
+                        result_folder="", white_ppo_path=white_ppo_path, black_ppo_path=black_ppo_path)
 
 episode = Episode()
 # Play the game
